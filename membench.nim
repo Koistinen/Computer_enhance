3aef93aab14c9df91973f092ec0367d33310b71e
Urban Koistinen <koistinen@teo.se>
2024-05-13 20:08:58 +0200

    Timings varied too much, I supect sleep sometimes oversleeep.

--- membench.nim
+++ membench.nim
@@ -3,22 +3,22 @@ import std/os
 import std/monotimes
 
 const
-  sz1 = 1u64 shl 18
-  sz2 = 1u64 shl 10
+  sz1 = 1u64 shl 17
+  sz2 = 1u64 shl 18
   sz = sz1*sz2
 
 var b = newSeq[int8](sz)
 var start = getMonoTime().ticks
-sleep(1000)
+sleep(10000)
 var stop = getMonoTime().ticks
 
-let ticksPerSecond = stop-start
-
+let ticksPerSecond = (stop-start).float/10.0
+echo ticksPerSecond, " ticks per second."
 start = getMonoTime().ticks
 for i in 0..(sz-1):
   b[i] = 1
 stop = getMonoTime().ticks
-echo sz.float*ticksPerSecond.float/(stop-start).float/1000000000.0
+echo sz.float*ticksPerSecond.float/(stop-start).float/1E6, "MB/s (in order writes)"
 
 var t = 1u64
 
@@ -30,4 +30,4 @@ stop = getMonoTime().ticks
 
 if t == 0: echo "Zero!"
 else: echo $sz & " " & "done."
-echo sz.float*ticksPerSecond.float/(stop-start).float/1000000000.0
+echo sz.float*ticksPerSecond.float/(stop-start).float/1E6, "MB/s (out of cache reads)"
