8f1dcc25d870eb8e505a50e86f7a1a0db02fc68a
Urban Koistinen <koistinen@teo.se>
2024-05-13 20:22:46 +0200

    Order of magnitude error + readability of results.

--- membench.nim
+++ membench.nim
@@ -3,22 +3,28 @@ import std/os
 import std/monotimes
 
 const
-  sz1 = 1u64 shl 17
-  sz2 = 1u64 shl 18
+  sz1 = 1u64 shl 24
+  sz2 = 1u64 shl 7
   sz = sz1*sz2
 
 var b = newSeq[int8](sz)
 var start = getMonoTime().ticks
-sleep(10000)
+sleep(1000)
 var stop = getMonoTime().ticks
 
-let ticksPerSecond = (stop-start).float/10.0
-echo ticksPerSecond, " ticks per second."
+let ticksPerSecond = (stop-start).float/1.0
+echo ticksPerSecond/1e6, " Mticks per second."
+start = getMonoTime().ticks
+for i in 0..(sz-1):
+  b[i] = 3
+stop = getMonoTime().ticks
+echo sz.float*ticksPerSecond.float/(stop-start).float/1E6, " MB/s (in order writes)"
+
 start = getMonoTime().ticks
 for i in 0..(sz-1):
   b[i] = 1
 stop = getMonoTime().ticks
-echo sz.float*ticksPerSecond.float/(stop-start).float/1E6, "MB/s (in order writes)"
+echo sz.float*ticksPerSecond.float/(stop-start).float/1E6, " MB/s (in order writes again)"
 
 var t = 1u64
 
